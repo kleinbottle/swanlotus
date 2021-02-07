04d36affbf8854bfc3bbe34d1b713f35bbbe6365
nandac <navanitachora@gmail.com>
2021-02-06 16:44:57 -0800

    Added script to install pandoc on Netlify and other minor changes

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://swanlotus.netlify.app'
+SITEURL = "https://swanlotus.netlify.app"
 RELATIVE_URLS = False
 
 # FEED_ALL_ATOM = 'feeds/all.atom.xml'
@@ -20,5 +20,5 @@ DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
-#DISQUS_SITENAME = ""
-#GOOGLE_ANALYTICS = ""
+# DISQUS_SITENAME = ""
+# GOOGLE_ANALYTICS = ""
