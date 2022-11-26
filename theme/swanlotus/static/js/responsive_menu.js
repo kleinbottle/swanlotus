92239a7ced82c29110b44f6cf7b4bd19013789c2
nandac <navanitachora@gmail.com>
2022-11-26 11:59:26 +0530

    Updated Bulma to version 0.9.4 and modified id names to use dashes

--- theme/swanlotus/static/js/responsive_menu.js
+++ theme/swanlotus/static/js/responsive_menu.js
@@ -1,20 +1,25 @@
 // Requires jQuery
 // Using JavaScript example from https://bulma.io/documentation/components/navbar/#navbar-menu
-$(document).ready(function() {
+$(document).ready(function () {
     // Check for click events on the navbar burger icon
-    $("#navBurger").click(function() {
+    $("#nav-small-screens").click(function () {
 
         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        $("#navBurger").toggleClass("is-active");
-        $("#navMenu").toggleClass("is-active");
+        $("#nav-small-screens").toggleClass("is-active");
+        $("#nav-ribbon-menu").toggleClass("is-active");
     });
 
-    $("#tocBurger").click(function(){
-        $("#mobile-contents").toggleClass("is-hidden");
-        if ($("#mobile-contents").is(":hidden") === true) {
-            $("#tocBurger").attr("title", "Expand contents")
-        } else if ($("#mobile-contents").is(":visible") === true) {
-            $("#tocBurger").attr("title", "Collapse contents")
+    $("#toc-small-screens").click(function () {
+        // Show or hide the table of contents on mobile devices when
+        // burger icon is clicked
+        $("#table-of-contents-small-screens").toggleClass("is-hidden");
+
+        // Modify the tooltip to Expand contents or Collapse contents
+        // depending on if the contents is hidden or visible
+        if ($("#table-of-contents-small-screens").is(":hidden") === true) {
+            $("#toc-small-screens").attr("title", "Expand contents")
+        } else if ($("#table-of-contents-small-screens").is(":visible") === true) {
+            $("#toc-small-screens").attr("title", "Collapse contents")
         }
     });
 });
