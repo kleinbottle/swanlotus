bb39c5ecff06c1b4d028b124aadeaf8580bc1392
kleinbottle <chyavana@gmail.com>
2023-10-21 23:15:39 +0530

    Updated website content

--- pelicanconf.py
+++ pelicanconf.py
@@ -63,7 +63,7 @@ DIRECT_TEMPLATES = ["index", "categories", "tags", "archives"]
 
 PATH = "content"
 ARTICLE_PATHS = [
-    "blogs"
+    "blogs/radians"
 ]  # draft value: ["blogs/rust-euler-one"]
 ARTICLE_SAVE_AS = ARTICLE_LANG_SAVE_AS = "blogs/{slug}.html"
 ARTICLE_URL = ARTICLE_LANG_URL = "blogs/{slug}.html"
--- theme/swanlotus/static/js/mathjax-config.js
+++ theme/swanlotus/static/js/mathjax-config.js
@@ -1,9 +1,9 @@
 window.MathJax = {
     chtml: {
-        scale: 0.91
-        // matchFontHeight: true
+        scale: 0.91 // Looks nice with Noto Serif and STIX2
+        // matchFontHeight: true // A smidgen too large
     },
     output: {
-        font: 'mathjax-stix2'
+        font: 'mathjax-stix2' // Needs MatJax 4 and above
     }
 };
