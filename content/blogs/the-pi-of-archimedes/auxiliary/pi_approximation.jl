a23ffe3882cd9c947c08b1b947f1a0c35a087458
nandac <navanitachora@gmail.com>
2024-07-10 19:43:27 +0530

    Updated pi_approximation.jl with author and date information

--- content/blogs/the-pi-of-archimedes/auxiliary/pi_approximation.jl
+++ content/blogs/the-pi-of-archimedes/auxiliary/pi_approximation.jl
@@ -1,32 +1,30 @@
 #!/usr/bin/env -S julia -t 4
-# Approximate π
-using Printf
-using Pkg
-
-# Install the PrettyTables package
-Pkg.add("PrettyTables", io=devnull)
+#
+# Author: Nandakumar Chandrasekhar
+# Date: 10 July 2024
+#
+# Approximate π to 10 decimal places by increasing the number
+# of sides on an n-sided polygon that is inscribing and circumscribing a circle
 using PrettyTables
 
 # Number of sides of the polygon inscribing and circumscribing the circle
 const NUM_SIDES = [6, 12, 24, 48, 96, 100, 1000, 10000, 100000, 1000000]
 
 function main()
-    # Approximate π using an inscribed and circumscribed n-sided polygon
-
     # Initialize a Float Matrix with unlimited rows and 3 columns
     pi_approximations = Array{Float64}(undef, 0, 3)
 
     for n in NUM_SIDES
         # Calculate the approximation for π using the inscribed circle
-        sin_approximation = round(n * sin(pi / n), sigdigits=11)
+        sin_approximation = round(n * sin(π / n), sigdigits=11)
 
         # Calculate the approximation for π using the circumscribed circle
-        tan_approximation = round(n * tan(pi / n), sigdigits=11)
+        tan_approximation = round(n * tan(π / n), sigdigits=11)
 
         # Append a vector containing the number of sides (n)
         # and the sine and tan approximations
         pi_approximations = vcat(
-            pi_approximations, reshape([n, sin_approximation, tan_approximation], 1, 3)
+            pi_approximations, reshape([n, sin_approximation, tan_approximation], 1, 3),
         )
     end
 
@@ -37,19 +35,19 @@ function print_table(pi_approximations)
     # Highlight the side column
     hl_side_col = Highlighter(
         f=(data, i, j) -> j == 1,
-        crayon=Crayon(foreground=:green)
+        crayon=Crayon(foreground=:green),
     )
 
     # Hightlight the sine approximation column
     hl_sin_col = Highlighter(
         f=(data, i, j) -> j == 2,
-        crayon=Crayon(foreground=:cyan)
+        crayon=Crayon(foreground=:cyan),
     )
 
     # Highlight the tan approximation column
     hl_tan_col = Highlighter(
         f=(data, i, j) -> j == 3,
-        crayon=Crayon(foreground=:magenta)
+        crayon=Crayon(foreground=:magenta),
     )
 
     # Print a table containing the approximations for π
@@ -60,7 +58,7 @@ function print_table(pi_approximations)
         highlighters=(hl_side_col, hl_sin_col, hl_tan_col), # Column value color
         header_crayon=Crayon(foreground=:yellow, bold=:true), # Header row color
         crop=:none, # Stop pretty_table from truncating rows
-        tf=tf_markdown
+        tf=tf_markdown,
     )
 end
 
