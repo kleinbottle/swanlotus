498e2161c999dcd4af06f7ec0855fd06c26cf9f9
nandac <navanitachora@gmail.com>
2024-07-13 14:52:56 +0530

    Add TeX markup for headings

--- content/blogs/the-pi-of-archimedes/auxiliary/pi_approximations.jl
+++ content/blogs/the-pi-of-archimedes/auxiliary/pi_approximations.jl
@@ -85,7 +85,7 @@ function print_table(pi_approximations)
         pi_approximations;
         formatters=(ft_printf("\$%d\$", 1), ft_printf("\$%11.10f\$", 2:5)),
         alignment = [:r, :r, :r, :r, :r],
-        header=(["\$n\$", "\$nsinθ\$", "\$ntanθ\$", "\$nsinθcosθ\$", "\$ntanθ\$"]),
+        header=(["\$n\$", "\$n\\sin\\frac{180°}{n}\$", "\$n\\tan\\frac{180°}{n}\$", "\$n\\sin\\frac{180°}{n}\\cos\\frac{180°}{n}\$", "\$n\\tan\\frac{180°}{n}\$"]),
         backend = Val(:markdown)
     )
 end
