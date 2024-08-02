ac79f4422b37f96172a1de37896efdababa09473
kleinbottle <chyavana@gmail.com>
2024-08-02 22:30:27 +0530

    Updated website content

--- pelicanconf.py
+++ pelicanconf.py
@@ -53,7 +53,7 @@ DIRECT_TEMPLATES = ["index", "categories", "tags", "archives"]
 IGNORE_FILES = ["blank-template.bib", "blog-template.md"]
 
 PATH = "content"
-ARTICLE_PATHS = ["blogs/radians", "blogs/pi-of-archimedes", "blogs/wonder-that-is-pi"]
+ARTICLE_PATHS = ["blogs/radians", "blogs/pi-of-archimedes", "blogs/wonder-that-is-pi", "blogs/same-action-four-castes"]
 
 ARTICLE_SAVE_AS = ARTICLE_LANG_SAVE_AS = "blogs/{slug}.html"
 ARTICLE_URL = ARTICLE_LANG_URL = "blogs/{slug}.html"
